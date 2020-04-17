c4d06bd065663fa1f19ac8154cb24ca326c25988
ildarnarbek <narbekovildar13@gmail.com>
2020-04-17 05:15:03 +0400

    first step of test passed

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,20 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+	@new_array=Array.new
+	puts "#{array}"
+	puts "#{array.max}"
+	array.each {|item|
+	puts item
+	if item > 0
+	puts "#{item}>0"  
+	@new_array << array.max
+	else
+	@new_array << item
+	end
+}
+puts "#{@new_array}"
+return @new_array
       end
 
       def search(_array, _query)
