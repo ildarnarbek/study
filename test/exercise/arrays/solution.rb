64add05504ca43f090a42b7573b352f237203513
ildarnarbek <narbekovildar13@gmail.com>
2020-04-18 21:25:55 +0400

    all tests passed, rubocop autofix

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,39 +2,26 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-	@new_array=Array.new
-	array.each {|item|
-	if item > 0  
-	@new_array << array.max
-	else
-	@new_array << item
-	end
-	}
-	return @new_array
+        @new_array = []
+        array.each do |item|
+          @new_array << (item.positive? ? array.max : item)
+        end
+        @new_array
       end
 
-      def search(_array, _query)
-	@left = -1
-	@right = _array.length
-	last_index = _array.length-1
-	last_item = _array[last_index]
-	puts " \n #{_array[0..10]} q= #{_query} l= #{@left} r =#{@right} last #{_array[-1]}"
-	return -1 if _array.length == 0 or last_item < _query
-	while @left < @right - 1
-	puts "start l = #{@left} r = #{@right}"  
-	@middle_index = (@left+@right)/2.ceil	
-	if _array[@middle_index] < _query    
-	@left = @middle_index
-	puts "middle_index #{@middle_index} .. middle_item #{_array[@middle_index]} < #{_query} l = #{@left}"
-	else	 
-	@right = @middle_index
-	puts "middle_index #{@middle_index} .. middle_item #{_array[@middle_index]} > #{_query} r= #{@right}"
-	end
-	puts "end l = #{@left} r= #{@right}"
-	end
-	return _array[@right] == _query ? @right : -1
-	
-	end
+      def search(array, query)
+        @left = -1
+        @right = array.length
+        while @left < @right - 1
+          @middle_index = (@left + @right) / 2.ceil
+          if array[@middle_index] < query
+            @left = @middle_index
+          else
+            @right = @middle_index
+          end
+        end
+        array[@right] == query ? @right : -1
+      end
     end
   end
 end
