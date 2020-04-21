a1f6ee79e66ede8f6885c9872ea3a59da7d2dc5b
ildarnarbek <narbekovildar13@gmail.com>
2020-04-21 16:15:31 +0400

    first step fp ex1

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,13 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def rating(array)
+        filtred_films = array.select{|film|  film["country"].to_s.include?(",") && !film["rating_kinopoisk"].nil? && film["rating_kinopoisk"].to_f > 0}
+        all_ratings = filtred_films.map {|film| film["rating_kinopoisk"].to_f}
+        quantity_ratings = all_ratings.length
+        
+        puts "#{all_ratings}"
+        puts "#{quantity_ratings}"
       end
 
       def chars_count(_films, _threshold)
