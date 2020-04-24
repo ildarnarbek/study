12411345d078a83ca1bdb8417fe879e69e69d8c8
ildarnarbek <narbekovildar13@gmail.com>
2020-04-25 01:31:18 +0400

    all test passed

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -8,13 +8,14 @@ module Exercise
         filtred_films = array.select{|film|  film["country"].to_s.include?(",") && !film["rating_kinopoisk"].nil? && film["rating_kinopoisk"].to_f > 0}
         all_ratings = filtred_films.map {|film| film["rating_kinopoisk"].to_f}
         quantity_ratings = all_ratings.length
-        
-        puts "#{all_ratings}"
-        puts "#{quantity_ratings}"
+        sum_ratings = all_ratings.reduce(:+)
+        average = sum_ratings / quantity_ratings
       end
 
-      def chars_count(_films, _threshold)
-        0
+      def chars_count(films, threshold)
+        filtred_films = films.select{|film| film["rating_kinopoisk"].to_f >= threshold }
+        films_names = filtred_films.map {|film| film["name"].to_s}
+        number_of_chars = films_names.map {|name| name.count('и')}.reduce(:+)
       end
     end
   end
