aef13235326f5b038620c863d07d238d3694176e
ildarnarbek <narbekovildar13@gmail.com>
2020-05-01 00:48:24 +0400

    my_reduce recursion cleaned works

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -14,7 +14,7 @@ module Exercise
 
       # Написать свою функцию my_map
       def my_map
-        #через reduce
+        # через reduce
         result_array = MyArray.new
         my_each { |item| result_array << yield(item) }
         result_array
@@ -22,38 +22,41 @@ module Exercise
 
       # Написать свою функцию my_compact
       def my_compact
-        #через reduce
+        # через reduce
         result_array = MyArray.new
         my_each { |item| result_array << item unless item.nil? }
         result_array
       end
 
-
-      def iter( acc, index, &block ) 
+      def iter(acc, index, &block)
         puts "\n \n enter acc #{acc} index #{index}"
-        item = self[index]
-        return acc if index == self.length
-        if acc.nil?
-          acc = item
-          index = 1
-        else 
-          acc = acc
-        end
+        return acc if index == length
+
+        # if acc.nil?
+        #   acc = self[index]
+        #   index = 1
+        # else
+        #   acc = acc
+        # end
+
+        # index = 1 if acc.nil?
+        # acc = self[0] if acc.nil?
+
+        acc.nil? ? acc = self[index] : acc = yield(acc, self[index])
+
         puts "\n def acc #{acc} index #{index}"
         puts "\n operation acc index #{acc} * #{self[index]}"
-        acc = yield( acc, self[index])
+        # acc = yield(acc, self[index])
         puts "res #{acc}"
-        index+=1
         puts "\n end iter \n"
-        return iter  acc, index, &block
+        iter(acc, index += 1, &block)
       end
-
 
       # Написать свою функцию my_reduce
       def my_reduce(acc = nil, &block)
-        acc = iter(acc,0,&block)
+        acc = iter(acc, 0, &block)
         puts "\n end reduce  acc #{acc}"
-        acc 
+        acc
       end
     end
   end
