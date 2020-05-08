4285879c5a64c86327b96b1dcb62c11c3618089b
ildarnarbek <narbekovildar13@gmail.com>
2020-05-08 05:32:45 +0400

    rackup works

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,7 +1,28 @@
 module Inatra
   class << self
-    def routes; end
+    @@handlers = {}
+    def routes(&block)
+    instance_eval(&block)
+    puts "******routes/// \n block #{block} \n"
+    end
 
-    def call(env); end
+
+    def call(env)
+      puts "!!!env #{env} "
+      request_method = env['REQUEST_METHOD'].to_sym.downcase
+      path = env['PATH_INFO']
+      puts "****method call \n  request #{request_method} path #{path}" 
+      puts "!!!!hand  #{@@handlers[request_method][path]}\n rm #{request_method} p #{path} \n  h met #{@@handlers[request_method]} \n #{@@handlers} "
+      return [404, {}, ['Not Found']] unless @@handlers[request_method][path]
+      @@handlers[request_method][path].call
+    end
+
+    def method_missing (method_name,*args,&block)
+      puts "met miss met_name #{method_name} args #{args}  block #{block} "
+      @@handlers[method_name] ||=  {} 
+      @@handlers[method_name][args[0]] = block
+      puts "handlers #{@@handlers}"
+    end
+
   end
 end
--- test/exercise/rackup/my_app.rb
+++ test/exercise/rackup/my_app.rb
@@ -1,13 +1,16 @@
 Inatra.routes do
   get '/hello' do
+    puts "******get hello"
     [200, {}, ['Hello World']]
   end
 
   get '/ping' do
+    puts "******get ping"
     [200, {}, ['PONG']]
   end
 
   post '/bye' do
+    puts "******post bye"
     [200, {}, ['Bye Bye']]
   end
 end
